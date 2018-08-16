63cc2f3e72cdecf5ef674828cf64904eb295a9db
Jozue06 <jozue06@gmail.com>
2018-08-16 12:59:03 -0700

    tesss

--- __tests__/src/action.test.js
+++ __tests__/src/action.test.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import reducer from '../../../src/reducer/category-reducer.js'
+import { categoryCreate, categoryDestroy, categoryUpdate } from '../../../src/action/actions.js'
 
-import App from '../../../src/main.js';
+//check if state initializes
+// if you pass it some unknown action it will pass back the default which is just the state
+//first argument is the state the second argument is the action
+//second argument can be an action thats either a string or a POJO
 
-describe('<App/> (Enzyme Test)', () => {
-  it('is alive at application start', () => {
-    let app = shallow(<App/>);
-    expect(app.find('main').exists()).toBeTruthy();
-    expect(app.find('Header').exists()).toBeTruthy();
-  });
-});
+describe('testing the reducer', ()=> {
+    it('Test: return orignal state on passing in an incorrect/invalid action', () => {
+        const red = reducer([], 'wrong')
+        expect(red).toEqual([])
+    })
 
-describe('<App/> (Snapshot Test)', () => {
-  it('renders right', () => {
-    const component = renderer.create(
-      <App />
-    );
+    it('Test: CATEGORY_CREATE', () => {
+        let action = {
+            type: 'CATEGORY_CREATE',
+            payload: {
+                id : 1,
+                content : 'hello'
+            }
+        }
+        let state = [{
+            id : 1,
+            content : 'hello'
+        }]
 
-    let tree = component.toJSON();
+        const red = reducer([], action)
+        expect(red).toEqual(state)
+    })
 
-    expect(tree).toMatchSnapshot();
+    it('Test: CATEGORY_UPDATE', () => {
+        let action = {
+            type: 'CATEGORY_UPDATE',
+            payload: {
+                id : 1,
+                content : 'bye'
+            }
+        }
 
-  });
-});
+        let oldState = [{
+            id : 1,
+            content : 'hello'
+        }]
+
+        let newState = [{
+            id : 1,
+            content : 'bye'
+        },
+        ]
+        const red = reducer(oldState, action)
+        expect(red).toEqual(newState)
+    })
+
+    it('Test: CATEGORY_DESTROY', () => {
+        
+        let action = {
+            type: 'CATEGORY_DESTROY',
+            payload: 1
+        }
+        let state = [{
+            id : 1,
+            content : 'hello'
+        }]
+        const red = reducer(state, action)
+        expect(red).toEqual([])
+    })
+})
